a5b369fefb14bd78953747595272b88897e77b8d
Bhim Magar <shokokomichan098@gmail.com>
2025-03-03 16:48:30 +0545

    initial

--- kim-miso-react/src/App.jsx
+++ kim-miso-react/src/App.jsx
@@ -1,14 +1,18 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Button from './Function.jsx'
+// import Header from './Header.jsx'
+// import Footer from './Footer.jsx'
+// import Orange from './Function.jsx'
+
+// === importing the Card
+// import Card from './Card.jsx'
+// === importing the props 
+// import Student from './Student.jsx'
+// import UserGreeting from'./UserGreeting.jsx'
+  // import Center from './MisoPro.jsx'
 
 function App() {
   return (
     <>
 
-    <Header />
-    <Footer />
-    <Button />
 
   </>
   );
